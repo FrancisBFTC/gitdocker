eb47414c6453a91cc3b2e9cfe77605e47b3973a6
FrancisBFTC <bftcorporations@gmail.com>
2022-08-18 21:35:50 -0300

    1ª versão do gitdocker

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+bool contains(char*, string);
+
+
+int main(int argc, char** argv) {
+	
+	FILE *file;
+	if(argc > 1){
+		if(contains(argv[1], ".txt")){
+			if((file = fopen(argv[1], "r")) == NULL){
+				printf("\nErro em abrir o arquivo para leitura!\n");
+				return 1;	
+			}
+			
+			char line[1024];
+			printf("\nArquivo aberto com sucesso\n");
+			while(fgets(line, sizeof(line), file) != NULL){
+				printf("%s", line);
+			}
+			
+			
+			fclose(file);
+		}else{
+			printf("\nO arquivo nao e um arquivo de texto!\n");
+		}
+		
+		
+	}else{
+		printf("O GitDocker espera ao menos 1 argumento");
+	}
+	
+	return 0;
+}
+
+string returnString(char *str){
+	stringstream data1;
+	data1 << str;
+	return data1.str();
+}
+
+bool contains(char* first, string second)
+{
+	 int pos = 0;
+     pos = returnString(first).find(second);
+	 
+     if(pos == -1)
+         return false;
+     else
+         return true;    
+          
+}
