988bc0a6bdbc6b90141e161635518a56c599c966
FrancisBFTC <bftcorporations@gmail.com>
2022-08-20 23:38:12 -0300

    Cria função configInterpreter() do param --config
    Nesta versão foi criado a função readTillComments() que lê um arquivo até a abertura de um comentário chamado dentro do configInterpreter() que vai processar o comando @path lendo o nome de cada arquivo. Também foram adicionadas o indexOf() e o substring() em linguagem C

---
+++ file.c
@@ -0,0 +1,9 @@
+/*	
+	Author: Wender Francis
+	GitDocker v0.0.3
+	
+	@path teste1.txt
+	@path teste2.txt
+	@path teste3.txt
+
+*/
--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
-/*
-	Author: Wender Francis
+/*   Author: Wender Francis
 	GitDocker v0.0.3
 	
 	@branch gitdocker-v0.0.3
@@ -21,13 +20,18 @@ using namespace std;
 bool contains(char*, string);
 void printJSONConfig();
 void showInfoHelp();
+void configInterpreter(char*);
+void readTillComments(FILE*, char[ ]);
+
+bool reading = false;
 
 //@description Adiciona 2 argumentos no MAIN
 int main(int argc, char** argv) {
 	
 	if(argc > 1){
 		if(strcmp(argv[1], "--config") == 0 || strcmp(argv[1], "-c") == 0){
-			printf("\nConfiguracao do --config\n");
+			printf("\nConfigurando Projeto:\n\n");
+			configInterpreter(argv[2]);
 			/*
 				TODO: Ler configurações iniciais de um arquivo e salva isso,
 				onde estas configurações poderão ser informações para um README,
@@ -64,7 +68,17 @@ bool contains(char* first, string second)
           
 }
 
-// @commit Função pra imprimir dados da config JSON
+// @description Função que pega uma parte de uma String
+char *substring(char *dst, const char *src, size_t start, size_t end){
+    return strncpy(dst, src + start, end);
+}
+
+// @description Função que pega índice de uma String em outra String
+int indexOf(char *a, const char *str){
+	return strstr(a, str) - a;
+}
+
+// @description Função pra imprimir dados da config JSON
 void printJSONConfig(){
 		FILE *file;
 		if((file = fopen("configs/config.json", "r")) == NULL){
@@ -81,11 +95,70 @@ void printJSONConfig(){
 		fclose(file);	
 }
 
-// @commit Função para imprimir dados de ajuda
+// @description Função para imprimir dados de ajuda
 void showInfoHelp(){
 	printf("\nGitDocker v0.0.3 Build 202208 \nCriado por Francis (KiddieOS Community)\n");
 	printf("Software de Documentacao e versionamento automatizado\n");
 	printf("\nusage: gitdocker [--config <arquivo>] [--show-config] \n\n");
 	printf("--config | -c <arquivo> : Define codigo principal onde contem configuracoes iniciais\n");
 	printf("--show-config | -sc : Exibe informacoes das configuracoes JSON \n\n");
+}
+
+// @description Função pra configurar interpretador
+void configInterpreter(char *source){
+		FILE *file;
+		if((file = fopen(source, "r")) == NULL){
+			printf("\nO arquivo de codigo-fonte nao existe!\n");
+			return;	
+		}
+			
+		char line[1024];
+		
+		readTillComments(file, line);
+		
+		while(reading){
+			
+			while(!contains(line, "@")){
+				char *value = fgets(line, sizeof(line), file);
+				
+				if(contains(line, "*/") || value == NULL){
+					reading = false;
+					break;
+				}
+			}
+			
+			if(reading){
+				if(contains(line, "@path ")){
+					int lengthCmd = strlen("@path")+indexOf(line, "@path")+1;
+					char *strFile = (char*) malloc(strlen(line));
+					substring(strFile, line, lengthCmd, indexOf(line, "\n"));
+					strFile[strlen(strFile)-1] = 0;
+					
+					printf("@path log: Registrando '%s' para leitura\n", strFile);
+					free(strFile);
+				}
+				
+				fgets(line, sizeof(line), file);
+			}
+			
+		}
+		
+		
+			
+		fclose(file);	
+}
+
+// @description Leitura de linhas de um arquivo até determinado caractere
+void readTillComments(FILE *file, char *line){
+	reading = false;
+	while(!reading){
+		char *value = fgets(line, sizeof(line), file);
+		if(value != NULL){
+			if(contains(line, "/*") || contains(line, "//")){
+				reading = true;
+			}
+		}else{
+			break;
+		}
+	}
 }
