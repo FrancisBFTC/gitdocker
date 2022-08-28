1b3acfed71593a24676e6c6d96e5f20d0734bd45
FrancisBFTC <bftcorporations@gmail.com>
2022-08-28 00:34:48 -0300

    Adicionando funcionalidade Y
    * Esta e uma nova funcionalidade

--- examples/commit/test.js
+++ examples/commit/test.js
@@ -2,3 +2,8 @@
     @commit Adicionando funcionalidade X
     @description Esta funcionalidade faz X, Y e Z
 */
+
+/*
+  @commit Adicionando funcionalidade Y
+  @description Esta e uma nova funcionalidade
+*/
