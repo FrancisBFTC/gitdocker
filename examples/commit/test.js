d2522526a80ef7c2fce1ac002831bb90b23ae26d
FrancisBFTC <bftcorporations@gmail.com>
2022-08-28 00:06:25 -0300

    Mensagem de commit lang.gitdock 1
    * descricao 1 do commit 1
    
    * descricao 2 do commit 1

--- examples/commit/test.js
+++ examples/commit/test.js
@@ -1,4 +1,4 @@
 /*
-  //  @commit Adicionando funcionalidade X
-  //  @description Esta funcionalidade faz X, Y e Z
+    @commit Adicionando funcionalidade X
+    @description Esta funcionalidade faz X, Y e Z
 */
